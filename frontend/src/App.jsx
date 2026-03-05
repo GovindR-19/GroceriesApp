b62a08d3267af32f10ba01a88bd8d61064f30c63
Govind <rummungovind@gmail.com>
2026-03-05 16:03:42 +0400

    Added Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "../src/CSS/grocery.module.css";
+import { useState, useEffect } from "react";
 
+
+function GroceriesItem({ name, count, toggle }){
+  const [value, setValue] = useState(count);  
+  const [itemName, setItemName] = useState(name);
+
+  const [toggleColor, setToggleColor] = useState("yellow");
+  const [toggleStatus, setToggleStatus] = useState(toggle);
+
+  
+  const handleToggle = ()=>{
+    setToggleStatus(!toggleStatus);
+  }
+
+  useEffect(()=>{
+    if(toggleColor === "yellow"){setToggleColor("white")}
+    else{ setToggleColor("yellow")}
+
+  },[toggleStatus])
+  
+
+  const handleInput = (event)=>{
+    setItemName(event.target.value);
+  }
+  
+  
+  const handleAdd = ()=>{
+    setValue(prev=> prev + 1)
+  }
+  
+  const handleSubstract = ()=>{
+    if(value>0){
+      setValue(prev=> prev - 1)
+    }
+  }
+
+  
+  return(
+    <div className={styles.tile} >
+      <button className={styles.toggle} onClick={handleToggle} style={{backgroundColor: toggleColor}} />
+      
+      <input  
+              className={styles.itemName} 
+              onChange={handleInput}
+              value={itemName} 
+              />
+
+      <button onClick={handleSubstract} >-</button>
+      <h1 className={styles.itemCount} >{value}</h1>
+      <button onClick={handleAdd} >+</button>      
+    </div>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [itemList, setItemList] = useState([{id:0, name: "new", category:"default", count: 1, toggle:true  }])
+
+  
+  const addItem =()=>{
+    const newId = itemList.length;
+    const newItem = [...itemList, {id: newId, name: "new", count:1, category: "default", toggle: true}];
+    setItemList(newItem);
+    
+  }
+
+ 
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container} >
+        <h1>Main Window</h1>
+        {
+          itemList.map( (item)=>{
+            return(
+                <GroceriesItem key={item.id} name={item.name} count={item.count} />
+            )
+          })
+        }
+        
+        <button onClick={addItem} className={styles.newBtn}>New</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     
   )
 }
 
