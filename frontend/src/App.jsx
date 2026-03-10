bd0ade27c5ec752826252bfcbc21d8d355aa8be9
Govind <rummungovind@gmail.com>
2026-03-10 15:52:43 +0400

    Finalised functionality and started DB connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import styles from "../src/CSS/grocery.module.css";
-import { useState  } from "react";
+import { useState } from "react";
 
 
-function GroceriesItem({name, count, deleteItem, addCount, removeCount, changeName}){
+function GroceriesItem({name, count,toggle, deleteItem, addCount, removeCount, changeName, toggleSwitch}){
 
 
   return(
     <div className={styles.tile} >
-      <button  className={styles.toggle} />
+      <button onClick={()=>toggleSwitch()} className={styles.toggle} style={{backgroundColor: toggle ? "white" : "yellow" }} />
 
       <input  type= "text"
               className={styles.itemName}
@@ -24,27 +24,27 @@ function GroceriesItem({name, count, deleteItem, addCount, removeCount, changeNa
   )
 }
 function App() {
-  const toggleState = true
+  
   const [list, setList] = useState(
     [
 
       {
         itemCategory: "Breakfast",
         items: [
-          { id:1 , name:"sugar",  count: 1, toggle: toggleState},
-          { id:2 , name: "milk",  count: 2, toggle: toggleState},
-          { id:3 , name: "tea",  count: 3, toggle: toggleState},
-          { id:4 , name: "coffee",  count: 4, toggle: toggleState},
+          { id:1 , name:"sugar",  count: 1, toggle: false},
+          { id:2 , name: "milk",  count: 2, toggle: false},
+          { id:3 , name: "tea",  count: 3, toggle: false},
+          { id:4 , name: "coffee",  count: 4, toggle: false},
         ]
       },
 
       {
         itemCategory: "Cleaning Supplies",
         items: [
-          { id:5 , name:"soap",  count: 1, toggle: toggleState},
-          { id:6 , name: "brush",  count: 2, toggle: toggleState},
-          { id:7 , name: "sprayer",  count: 3, toggle: toggleState},
-          { id:8 , name: "platic bag",  count: 4, toggle: toggleState},
+          { id:5 , name:"soap",  count: 1, toggle: false},
+          { id:6 , name: "brush",  count: 2, toggle: false},
+          { id:7 , name: "sprayer",  count: 3, toggle: false},
+          { id:8 , name: "platic bag",  count: 4, toggle: false},
         ]
       },
     ]
@@ -56,7 +56,7 @@ function App() {
       ...newList[categoryIndex],
       items: [
         ...newList[categoryIndex].items,
-        {id:Date.now() ,name: "New", count:1, toggle: toggleState}
+        {id:Date.now() ,name: "New", count:1, toggle: false}
       ]
     } 
     setList(newList);
@@ -137,6 +137,38 @@ function App() {
 
   }
 
+  const toggleSwitch = ( categoryIndex, itemIndex)=>{
+    const newList = [...list];
+    
+    newList[categoryIndex] = {
+      ...newList[categoryIndex],
+      items: newList[categoryIndex].items.map((item, i)=>{
+        if(i === itemIndex){
+            return {...item, toggle: !item.toggle}
+        }
+        return item;
+      })
+    }
+
+    setList(newList);
+
+  }
+
+  const addCategory = ()=>{
+    const newList = [...list];
+
+    const newCategory = {itemCategory: "New", items: [{id:Date.now() ,name: "New", count:1, toggle: false}] }
+
+    setList([...newList, newCategory]);
+
+  }
+
+  const deleteCategory = (categoryIndex)=>{
+    const newList = list.filter(( _ , i)=> i!== categoryIndex )
+
+    setList(newList);
+  }
+
 
 
  return(
@@ -146,29 +178,38 @@ function App() {
 
         return (
           <div>
-            <h2 key={categoryIndex} >{element.itemCategory}</h2>
-            <>{
+            <div key={categoryIndex} style={{display: "flex", alignItems: "center", justifyContent:"space-around",}} >
+              <h2  >{element.itemCategory}</h2>
+              <button onClick={()=>deleteCategory(categoryIndex)} >Delete All</button>
+            </div>
+
+            <div>{
               element.items.map((i, itemIndex)=>{
                 return (
                   <li key={itemIndex} >
                         <GroceriesItem
                         count={i.count}
                         name={i.name}
+                        toggle={i.toggle}
                         deleteItem={()=>deleteItem(categoryIndex, itemIndex)}
                         addCount={()=>incrementCount(categoryIndex, itemIndex)}
                         removeCount={()=>decrementCount(categoryIndex, itemIndex)}
                         changeName={(newName)=>changeItemName(categoryIndex, itemIndex, newName)}
+                        toggleSwitch={()=>toggleSwitch(categoryIndex, itemIndex)}
                         />
                   </li>
                 )
           })
-              }</>
+              }</div>
               <button className={styles.addBtn} onClick={()=>addItem(categoryIndex)} >Add</button>
           </div>
       )
       })
     }
-    <button>New Category</button>
+    <button onClick={()=>{addCategory()}} >New Category</button>
+    <br/><br/>
+    <button style={{width: "150px", position: "absolute", alignItems: "center"}} >Save</button>
+    <br/><br/><br/><br/><br/><br/><br/><br/>
   </div>
  )
 
