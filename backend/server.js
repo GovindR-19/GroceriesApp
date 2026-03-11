8f4c17db593c6934ac380589728389b84840c063
Govind <rummungovind@gmail.com>
2026-03-11 11:44:45 +0400

    Starting Server Side

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
+const pool = require("./db");
 const port = 3000
 
+server.use(cors());
+server.use(express.json());
+
 server.get("/", (request,response)=>{
-    response.send("App One");
+    response.send("Groceries App");
+});
+
+server.get("/test", (request, response)=>{
+    response.send("Testing, testing. Is this mike on?")
+});
+
+server.get('/items', async (request, response) =>{
+    try{
+        const allItems = await pool.query('SELECT * FROM items');
+        response.json(allItems.rows);
+    }
+
+    catch(err){
+        console.error(err.message);
+        response.status(800).send('Server Error');
+    }
 })
 
 server.listen(port, ()=>{
     console.log(`Server running at http://localhost:${port}/`);
-})
+});
